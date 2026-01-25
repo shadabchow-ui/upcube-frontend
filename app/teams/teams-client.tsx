95b7a1cf17138ab1aa109610b08bd9afa32d3cbd
shadabchow-ui <shadabchow@gmail.com>
2026-01-25 04:51:25 -0500

    Create teams-client.tsx

---
+++ app/teams/teams-client.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React, { useMemo, useState } from "react"
+
+export default function TeamsClient() {
+  return (
+    <div>
+      {/* move original JSX from page.tsx here */}
+    </div>
+  )
+}
